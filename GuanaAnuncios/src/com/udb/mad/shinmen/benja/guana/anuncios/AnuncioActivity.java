c5a0e815a602250388c3a01ab9824e2042434aeb
shinmen14 <shinmen14@gmail.com>
2013-09-17 12:11:22 -0600

    -Se agrega llamada a la actividad para publicar el anuncio -Layout inicial para la actividad para publicar anuncios

--- GuanaAnuncios/src/com/udb/mad/shinmen/benja/guana/anuncios/AnuncioActivity.java
+++ GuanaAnuncios/src/com/udb/mad/shinmen/benja/guana/anuncios/AnuncioActivity.java
@@ -1,8 +1,10 @@
 package com.udb.mad.shinmen.benja.guana.anuncios;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class AnuncioActivity extends ActionBarActivity {
 
@@ -18,5 +20,23 @@ public class AnuncioActivity extends ActionBarActivity {
 		getMenuInflater().inflate(R.menu.anuncio, menu);
 		return true;
 	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.action_nuevo_anuncio:
+	        	openPublicarAnuncioActivity();
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
 
+	private void openPublicarAnuncioActivity(){
+		Intent i = new Intent();
+		i.setClass(this, PublicarAnuncioActivity.class);
+		startActivity(i);
+	}
+	
 }
