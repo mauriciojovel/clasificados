0b972c06823acbdec4488f679b9d2ca4a80f1453
Benjamin A <jbenjamin.aguilar@gmail.com>
2013-09-20 17:06:49 -0600

    Creando widget y agregando layouts para el widget

---
+++ GuanaAnuncios/src/com/udb/mad/shinmen/benja/guana/anuncios/widget/GuanaAnuncioWidget.java
@@ -0,0 +1,15 @@
+package com.udb.mad.shinmen.benja.guana.anuncios.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class GuanaAnuncioWidget extends AppWidgetProvider{
+
+	 @Override
+	    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+	                 int[] appWidgetIds) {
+
+	    }
+
+}
