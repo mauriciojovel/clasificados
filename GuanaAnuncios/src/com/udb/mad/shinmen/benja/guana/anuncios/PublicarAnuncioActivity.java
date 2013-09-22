051adf39154627fb744969383675d793dd156a49
Carlos <shinmen14@gmail.com>
2013-09-22 16:24:07 -0600

    Limpieza de codigo e implementacion de la nueva modalidad para guardar anuncios a través de un servicio

--- GuanaAnuncios/src/com/udb/mad/shinmen/benja/guana/anuncios/PublicarAnuncioActivity.java
+++ GuanaAnuncios/src/com/udb/mad/shinmen/benja/guana/anuncios/PublicarAnuncioActivity.java
@@ -1,26 +1,21 @@
 package com.udb.mad.shinmen.benja.guana.anuncios;
 
-import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -32,7 +27,6 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
-import android.widget.Toast;
 
 import com.udb.mad.shinmen.benja.guana.anuncios.adapters.ImagenAnuncioCustomAdapter;
 import com.udb.mad.shinmen.benja.guana.anuncios.model.Anuncio;
@@ -92,7 +86,7 @@ public class PublicarAnuncioActivity extends ActionBarActivity implements
 		parametros.add(new BasicNameValuePair("usuario", usuario));
 		parametros.add(new BasicNameValuePair("token", token));
 		JSONDownloaderTask<JSONArray> jdt = new JSONDownloaderTask<JSONArray>(
-				"http://guananuncio.madxdesign.com/index.php/anuncio/categorias",
+				getResources().getString(R.string.categoriasService),
 				JSONDownloaderTask.METODO_POST, parametros, true);
 		jdt.setOnFinishDownload(new CategoriaDownloadListener());
 		jdt.execute();
@@ -151,19 +145,6 @@ public class PublicarAnuncioActivity extends ActionBarActivity implements
 	}
 
 	private void guardarAnuncio() {
-		/*List<NameValuePair> parametros = new ArrayList<NameValuePair>(8);
-		parametros.add(new BasicNameValuePair("usuario", usuario));
-		parametros.add(new BasicNameValuePair("token", token));
-		parametros.add(new BasicNameValuePair("titulo", this.edtTitulo
-				.getText().toString()));
-		parametros.add(new BasicNameValuePair("descripcion",
-				this.edtDescripcion.getText().toString()));
-		parametros.add(new BasicNameValuePair("categoria_id",
-				categoriaSeleccionada));
-		parametros.add(new BasicNameValuePair("precio", this.edtPrecio
-				.getText().toString()));
-		parametros.add(new BasicNameValuePair("telefono", this.edtTelefono
-				.getText().toString()));*/
 		
 		Anuncio anuncio = new Anuncio();
 		anuncio.setTituloAnuncio(this.edtTitulo.getText().toString());
@@ -184,111 +165,7 @@ public class PublicarAnuncioActivity extends ActionBarActivity implements
 			intent.putStringArrayListExtra(PublicarAnuncioService.ANUNCIO_IMAGENES_EXTRA, rutasImagenes);
 		}
 		startService(intent);
-		
 		finish();
-
-		//TODO esto debería borrarse
-		/*JSONDownloaderTask<JSONObject> jdt = new JSONDownloaderTask<JSONObject>(
-				"http://guananuncio.madxdesign.com/index.php/anuncio/save",
-				JSONDownloaderTask.METODO_POST, parametros);
-		jdt.setOnFinishDownloadJSONObject(new PublicarAnuncioListener());
-		jdt.setOnStartDownloadListener(new OnStartDownload() {
-			@Override
-			public void onStartDownload() {
-				pd = new ProgressDialog(activity);
-				pd.setTitle(getResources()
-						.getString(R.string.titulo_procesando));
-				pd.setMessage(getResources().getString(R.string.titulo_espere));
-				pd.setCancelable(false);
-				pd.setIndeterminate(true);
-				pd.show();
-			}
-		});*/
-		//jdt.execute();
-	}
-
-	private boolean subirImagenes(String idAnuncio) {
-
-		Bitmap bm = BitmapFactory.decodeFile(adapter.getItem(0), null);
-		ByteArrayOutputStream stream = new ByteArrayOutputStream();
-		bm.compress(Bitmap.CompressFormat.JPEG, 40, stream);
-		byte[] byte_arr = stream.toByteArray();
-		String image_str = Base64.encodeToString(byte_arr, Base64.DEFAULT);
-
-		List<NameValuePair> parametros = new ArrayList<NameValuePair>(4);
-		parametros.add(new BasicNameValuePair("usuario", usuario));
-		parametros.add(new BasicNameValuePair("token", token));
-		parametros.add(new BasicNameValuePair("imagen", image_str));
-		parametros.add(new BasicNameValuePair("anuncio_id", idAnuncio));
-
-		JSONDownloaderTask<JSONObject> jdt = new JSONDownloaderTask<JSONObject>(
-				"http://guananuncio.madxdesign.com/index.php/imagen/save",
-				JSONDownloaderTask.METODO_POST, parametros);
-		jdt.setOnFinishDownloadJSONObject(new SubirImagenListener());
-		jdt.execute();
-
-		return false;
-	}
-
-	private class SubirImagenListener implements
-			JSONDownloaderTask.OnFinishDownloadJSONObject<JSONObject> {
-		@Override
-		public void onFinishDownloadJSONObject(JSONObject jsonObject) {
-			pd.dismiss();
-			try {
-				String status = jsonObject.getString("estado");
-				if (status.equals("1")) {
-					Toast.makeText(activity, "Anuncio Publicado",
-							Toast.LENGTH_SHORT).show();
-					finish();
-				} else {
-					if (status.equals("-1")) {
-						Toast.makeText(
-								activity,
-								"Ocurrio un error inesperado. Vuelva a intentar.",
-								Toast.LENGTH_SHORT).show();
-					}
-				}
-			} catch (JSONException e) {
-				Log.e("error", e.getMessage());
-			}
-		}
-
-		@Override
-		public void loadError() {
-			// TODO Auto-generated method stub
-
-		}
-	}
-
-	private class PublicarAnuncioListener implements
-			JSONDownloaderTask.OnFinishDownloadJSONObject<JSONObject> {
-		@Override
-		public void onFinishDownloadJSONObject(JSONObject jsonObject) {
-			try {
-				String status = jsonObject.getString("estado");
-				if (status.equals("1")) {
-					String id = jsonObject.getString("id");
-					subirImagenes(id);
-				} else {
-					if (status.equals("-1")) {
-						pd.dismiss();
-						Toast.makeText(
-								activity,
-								"Ocurrio un error inesperado. Vuelva a intentar.",
-								Toast.LENGTH_SHORT).show();
-					}
-				}
-			} catch (JSONException e) {
-				Log.e("error", e.getMessage());
-			}
-		}
-
-		@Override
-		public void loadError() {
-			// TODO Auto-generated method stub
-
-		}
 	}
 
 	private class CategoriaDownloadListener implements
