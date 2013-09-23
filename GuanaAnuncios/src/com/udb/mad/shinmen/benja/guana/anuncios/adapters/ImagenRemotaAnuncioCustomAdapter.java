3dcc62901929fe095d93f98dce87dfd2f16d9567
shinmen14 <shinmen14@gmail.com>
2013-09-23 14:39:36 -0600

    Se cambio a utilizar la nueva clase utilitaria para carga de imagenes

--- GuanaAnuncios/src/com/udb/mad/shinmen/benja/guana/anuncios/adapters/ImagenRemotaAnuncioCustomAdapter.java
+++ GuanaAnuncios/src/com/udb/mad/shinmen/benja/guana/anuncios/adapters/ImagenRemotaAnuncioCustomAdapter.java
@@ -1,6 +1,9 @@
 package com.udb.mad.shinmen.benja.guana.anuncios.adapters;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,17 +11,29 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 
 import com.udb.mad.shinmen.benja.guana.anuncios.R;
-import com.udb.mad.shinmen.benja.guana.anuncios.utilidades.ImageDownloaderTask;
+import com.udb.mad.shinmen.benja.guana.anuncios.utilidades.ImageDownloader;
 import com.udb.mad.shinmen.benja.guana.anuncios.utilidades.PreferenciasUsuario;
 
 public class ImagenRemotaAnuncioCustomAdapter extends ArrayAdapter<String> {
 
 	private Activity activity;
+	private final ImageDownloader imageDownloader;
 	
 	public ImagenRemotaAnuncioCustomAdapter(Activity activity) {
 		super(activity, R.layout.imagen_anuncio_list_item);
 		this.activity = activity;
+
+        int scaleWidth = scaledImage(100, activity);
+        int scaleHeight = scaledImage(100, activity);
+		imageDownloader = new ImageDownloader(scaleWidth,scaleHeight);
 	}
+	
+	public int scaledImage(int px, Context context) {
+        Resources resources = context.getResources();
+        DisplayMetrics metrics = resources.getDisplayMetrics();
+        int px2 = (int) (px*metrics.density+0.5f);
+        return px2;
+    }
 	
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
@@ -37,9 +52,13 @@ public class ImagenRemotaAnuncioCustomAdapter extends ArrayAdapter<String> {
         String usuario = PreferenciasUsuario.getUsuario(activity);
 		String token = PreferenciasUsuario.getToken(activity);
 		url = url + "?usuario="+usuario+"&token="+token;
-		new ImageDownloaderTask(view).execute(url);
+		imageDownloader.download(url,view);
 		      
         return vi;
 	}
+	
+	public ImageDownloader getImageDownloader() {
+        return imageDownloader;
+    }
 	
 }
