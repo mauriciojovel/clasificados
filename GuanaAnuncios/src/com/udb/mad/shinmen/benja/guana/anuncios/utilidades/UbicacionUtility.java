583157cc9657c5177238006d797936b4d5b222b2
mauriciojovel <mauriciojovel@gmail.com>
2013-09-23 11:45:36 -0600

    - Se hicieron cambios en la utilidad de ubicación para evitar errores si no se tiene disponible gps. - Se le puso nombre a la aplicación.

--- GuanaAnuncios/src/com/udb/mad/shinmen/benja/guana/anuncios/utilidades/UbicacionUtility.java
+++ GuanaAnuncios/src/com/udb/mad/shinmen/benja/guana/anuncios/utilidades/UbicacionUtility.java
@@ -3,21 +3,34 @@ package com.udb.mad.shinmen.benja.guana.anuncios.utilidades;
 import android.content.Context;
 import android.location.Location;
 import android.location.LocationManager;
+import android.util.Log;
 
 public class UbicacionUtility {
     public static double getLongitud(Context ctx) {
-        LocationManager lm = (LocationManager) ctx
+        try {
+            LocationManager lm = (LocationManager) ctx
                                     .getSystemService(Context.LOCATION_SERVICE);
-        Location location = lm
-                .getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        return location.getLongitude();
+            Location location = lm
+                    .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            return location.getLongitude();
+        } catch(Exception e) {
+            Log.e("UbicacionUtility", "Pudo ser porque no tiene gps", e);
+            return 0.0;
+        }
     }
     
     public static double getAltitud(Context ctx) {
-        LocationManager lm = (LocationManager) ctx
-                .getSystemService(Context.LOCATION_SERVICE);
-        Location location = lm
-        .getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        return location.getLatitude();
+        
+        Location location;
+        try {
+            LocationManager lm = (LocationManager) ctx
+                    .getSystemService(Context.LOCATION_SERVICE);
+            location = lm
+            .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            return location.getLatitude();
+        } catch (Exception e) {
+            Log.e("UbicacionUtility", "Pudo ser porque no tiene gps", e);
+            return 0.0;
+        }
     }
 }
